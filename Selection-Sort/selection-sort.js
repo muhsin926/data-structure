72a881ebab4d40559ddf8882d5fb2585dc80ba15
muhsin926 <muhsinmp926@gmail.com>
2023-01-02 21:06:57 +0530

    optimazed insertion sort

--- Selection-Sort/selection-sort.js
+++ Selection-Sort/selection-sort.js
@@ -2,13 +2,18 @@ const selectionSort = (array) => {
   const n = array.length;
   let i, j;
   for (i = 0; i < n; i++) {
+    let swaping = false;
     let smallest = array[i];
     for (j = i + 1; j < n; j++) {
       if (array[j] < smallest) {
         smallest = array[j];
         array[j] = array[i];
         array[i] = smallest;
+        swaping = true;
       }
+    }
+    if (swaping == false) {
+      break;
     }
   }
   return array;
@@ -16,6 +21,5 @@ const selectionSort = (array) => {
 
 const arr = [20, 70, 59, 40, 60, 10];
 const result = selectionSort(arr);
-result.forEach((element) => {
-  console.log(element);
-});
+result.forEach((element) => console.log(element))
+
---
+++ sample.js
@@ -0,0 +1,9 @@
+const bubbleSort = (arr) =>{
+    for (i=0; i<n-1; i++){
+        for (j=0; j<n-i-1; j++){
+            if (arr[j]>arr[j+1]){
+                arr
+            }
+        }
+    }
+}
