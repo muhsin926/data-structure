af9311127fd846d671f50043475a9048ba80518b
muhsin926 <muhsinmp926@gmail.com>
2023-02-13 14:52:24 +0530

    learn min heap

---
+++ Heaps/operation.js
@@ -0,0 +1,10 @@
+function insert(heap,value){
+    heap.push(value);
+
+    let currIndx = heap.length-1;
+
+    while(currIndx > 0){
+        let parent = Math.floor((currIndx-1)/2);
+        
+    }
+}
